ea87175936783f2de6907490ea5b894b706c3fbc
Caique Pires <c.pires@ba.estudante.senai.br>
2025-07-14 22:16:48 +0100

    Created initial project

---
+++ src/test/java/com/petshop/petland_api/PetlandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshop.petland_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetlandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
