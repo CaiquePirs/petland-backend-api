4d23f0857a8c26218b4fbf804a01387af1ccc195
Caique Pires <c.pires@ba.estudante.senai.br>
2025-07-16 19:03:36 +0100

    feat: adding validation to the address class

--- src/main/java/com/petland/common/Address.java
+++ src/main/java/com/petland/common/Address.java
@@ -1,14 +1,35 @@
 package com.petland.common;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 
+    @NotBlank(message = "Street is required")
     private String street;
+
+    @NotBlank(message = "Number is required")
     private String number;
+
+    @NotBlank(message = "ZipCode is required")
     private String zipCode;
+
+    @NotBlank(message = "State is required")
     private String state;
-    private String contry;
+
+    @NotBlank(message = "Country is required")
+    private String country;
+
+    @NotBlank(message = "City is required")
     private String city;
+
 }
