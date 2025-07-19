ac83906730b78b3cb0faa5660143470fae0d7597
Caique Pires <c.pires@ba.estudante.senai.br>
2025-07-19 15:57:53 +0100

    feat: Create and customize standard error handling messages
    -Returns a pleasant message, error code, and a list of errors

--- src/main/java/com/petland/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/petland/common/handler/GlobalExceptionHandler.java
@@ -2,6 +2,10 @@ package com.petland.common.handler;
 
 import com.petland.common.error.ErrorMessageDTO;
 import com.petland.common.error.ErrorResponseDTO;
+import com.petland.common.exception.EmailFoundException;
+import com.petland.common.exception.InvalidCredentialsException;
+import com.petland.common.exception.UnauthorizedException;
+import com.petland.common.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -28,6 +32,54 @@ public class GlobalExceptionHandler {
                 listErrors
         );
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
+    }
+
+    @ExceptionHandler(EmailFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleEmailFound(EmailFoundException e){
+        ErrorMessageDTO errorMessage = new ErrorMessageDTO("Email", e.getMessage());
+
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                HttpStatus.CONFLICT.value(),
+                e.getMessage(),
+                List.of(errorMessage));
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleInvalidCredentials(InvalidCredentialsException e){
+        ErrorMessageDTO errorMessage = new ErrorMessageDTO("Credentials", e.getMessage());
+
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                HttpStatus.FORBIDDEN.value(),
+                e.getMessage(),
+                List.of(errorMessage));
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handlerUserNotFound(UserNotFoundException e){
+        ErrorMessageDTO errorMessageDTO = new ErrorMessageDTO("User not found", e.getMessage());
+
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                HttpStatus.NOT_FOUND.value(),
+                e.getMessage(),
+                List.of(errorMessageDTO));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorResponseDTO> handlerUnauthorized(UnauthorizedException e){
+        ErrorMessageDTO errorMessage = new ErrorMessageDTO("Unauthorized", e.getMessage());
+
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                HttpStatus.UNAUTHORIZED.value(),
+                e.getMessage(),
+                List.of(errorMessage));
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 
 }
