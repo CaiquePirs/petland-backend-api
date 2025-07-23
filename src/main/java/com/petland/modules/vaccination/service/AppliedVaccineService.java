12a953383642b714b13074d6ab8e837dd79dbfcb
Caique Pires <c.pires@ba.estudante.senai.br>
2025-07-23 22:52:23 +0100

    feat(vaccination): Implement route vaccination registration with vaccine administration and total cost calculation
    -Created a method in `AppliedVaccineService` to administer vaccines with inventory updates
    -Added `register` method to `VaccinationService` to record vaccinations
    including client, pet, veterinarian, vaccines administered, and total cost calculation
    -Integration between services: Pet, Customer, Employee, AppliedVaccine, and cost calculation

---
+++ src/main/java/com/petland/modules/vaccination/service/AppliedVaccineService.java
@@ -0,0 +1,38 @@
+package com.petland.modules.vaccination.service;
+
+import com.petland.enums.StatusEntity;
+import com.petland.modules.vaccination.dto.AppliedVaccineRequestDTO;
+import com.petland.modules.vaccination.module.Vaccination;
+import com.petland.modules.vaccination.module.Vaccine;
+import com.petland.modules.vaccination.module.AppliedVaccine;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AppliedVaccineService {
+
+    private final VaccineService vaccineService;
+
+    public List<AppliedVaccine> create(Vaccination vaccination, List<AppliedVaccineRequestDTO> listAppliedVaccineRequest){
+        List<AppliedVaccine> listVaccineApplied = listAppliedVaccineRequest.stream()
+                .map(appliedVaccineRequest -> {
+                    AppliedVaccine appliedVaccine = new AppliedVaccine();
+                    Vaccine vaccine = vaccineService.updateVaccineStock(appliedVaccineRequest.quantityUsed(), appliedVaccineRequest.vaccineId());
+                    appliedVaccine.setVaccination(vaccination);
+                    vaccination.getAppliedVaccines().add(appliedVaccine);
+
+                    appliedVaccine.setVaccine(vaccine);
+                    appliedVaccine.setQuantityUsed(appliedVaccineRequest.quantityUsed());
+                    appliedVaccine.setStatus(StatusEntity.ACTIVE);
+                    return appliedVaccine;
+                }).collect(Collectors.toList());
+
+        vaccination.setAppliedVaccines(listVaccineApplied);
+        return listVaccineApplied;
+    }
+
+}
---
+++ src/main/java/com/petland/modules/vaccination/service/VaccinationService.java
@@ -0,0 +1,55 @@
+package com.petland.modules.vaccination.service;
+
+import com.petland.enums.StatusEntity;
+import com.petland.modules.customer.model.Customer;
+import com.petland.modules.customer.service.CustomerService;
+import com.petland.modules.employee.model.Employee;
+import com.petland.modules.employee.service.EmployeeService;
+import com.petland.modules.pet.model.Pet;
+import com.petland.modules.pet.service.PetService;
+import com.petland.modules.vaccination.dto.VaccinationRequestDTO;
+import com.petland.modules.vaccination.module.Vaccination;
+import com.petland.modules.vaccination.module.AppliedVaccine;
+import com.petland.modules.vaccination.repository.VaccinationRepository;
+import com.petland.modules.vaccination.util.CalculateTotalVaccinationCost;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class VaccinationService {
+
+    private final EmployeeService employeeService;
+    private final CustomerService customerService;
+    private final PetService petService;
+    private final AppliedVaccineService appliedVaccineService;
+    private final CalculateTotalVaccinationCost calculateTotalVaccinationCost;
+    private final VaccinationRepository vaccinationRepository;
+
+    @Transactional
+    public Vaccination register(VaccinationRequestDTO vaccinationRequestDTO){
+        Vaccination vaccination = new Vaccination();
+        Pet pet = petService.findPetById(vaccinationRequestDTO.petId());
+        Customer customer = customerService.findCustomerById(vaccinationRequestDTO.customerId());
+        Employee veterinarian = employeeService.findById(vaccinationRequestDTO.veterinarianId());
+
+        vaccination.setPet(pet);
+        vaccination.setCustomer(customer);
+        vaccination.setVeterinarian(veterinarian);
+        vaccination.setLocation(vaccinationRequestDTO.location());
+        vaccination.setStatus(StatusEntity.ACTIVE);
+        vaccination.setClinicalNotes(vaccinationRequestDTO.clinicalNotes());
+        vaccination.setVaccinationDate(vaccinationRequestDTO.vaccinationDate());
+        vaccination.setNextDoseDate(vaccinationRequestDTO.nextDoseDate());
+
+        List<AppliedVaccine> listAppliedVaccine = appliedVaccineService.create(vaccination, vaccinationRequestDTO.listAppliedVaccineRequestDTO());
+        BigDecimal totalCostVaccination = calculateTotalVaccinationCost.calculate(listAppliedVaccine);
+        vaccination.setTotalCostVaccination(totalCostVaccination);
+
+        return vaccinationRepository.save(vaccination);
+    }
+}
