286c507597e6f41d69a5b0fcc19f3412adbe7a20
Caique Pires <c.pires@ba.estudante.senai.br>
2025-07-23 22:43:53 +0100

    feat(vaccination): Create a utility method to generate a response to the creation of a vaccination performed
    -receives a vaccination entity and returns a response DTO containing only the necessary information

---
+++ src/main/java/com/petland/modules/vaccination/util/GenerateVaccinationResponse.java
@@ -0,0 +1,49 @@
+package com.petland.modules.vaccination.util;
+
+import com.petland.modules.vaccination.dto.VaccinationResponseDTO;
+import com.petland.modules.vaccination.dto.AppliedVaccineResponseDTO;
+import com.petland.modules.vaccination.module.Vaccination;
+import com.petland.modules.vaccination.module.AppliedVaccine;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GenerateVaccinationResponse {
+
+    public VaccinationResponseDTO generate(Vaccination vaccination){
+        List<AppliedVaccineResponseDTO> listAppliedVaccines = generateAppliedVaccineListResponse(vaccination.getAppliedVaccines());
+
+        VaccinationResponseDTO responseDTO = VaccinationResponseDTO.builder()
+                .id(vaccination.getId())
+                .petId(vaccination.getPet().getId())
+                .customerId(vaccination.getCustomer().getId())
+                .veterinarianId(vaccination.getVeterinarian().getId())
+                .location(vaccination.getLocation())
+                .listAppliedVaccineResponseDTO(listAppliedVaccines)
+                .vaccinationDate(vaccination.getVaccinationDate())
+                .nextDoseDate(vaccination.getNextDoseDate())
+                .clinicalNotes(vaccination.getClinicalNotes())
+                .totalCostVaccination(vaccination.getTotalCostVaccination())
+                .build();
+        return responseDTO;
+    }
+
+    public List<AppliedVaccineResponseDTO> generateAppliedVaccineListResponse(List<AppliedVaccine> listAppliedVaccine){
+        List<AppliedVaccineResponseDTO> listAppliedVaccineDTO = new ArrayList<>();
+
+        for(AppliedVaccine appliedVaccine : listAppliedVaccine){
+            AppliedVaccineResponseDTO listResponse = AppliedVaccineResponseDTO.builder()
+                    .id(appliedVaccine.getId())
+                    .vaccinationId(appliedVaccine.getVaccination().getId())
+                    .vaccineId(appliedVaccine.getVaccine().getId())
+                    .quantityUsed(appliedVaccine.getQuantityUsed())
+                    .build();
+            listAppliedVaccineDTO.add(listResponse);
+        }
+        return listAppliedVaccineDTO;
+    }
+}
