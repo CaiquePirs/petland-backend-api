22ebe41949d49a1a105c9f2caeff29a020338023
Caique Pires <c.pires@ba.estudante.senai.br>
2025-07-24 00:48:55 +0100

    refactor(customer): Implement an endpoint for customers to search for their profile data.
    -also created an endpoint for employees to search for customers by ID.

--- src/main/java/com/petland/modules/customer/controller/CustomerController.java
+++ src/main/java/com/petland/modules/customer/controller/CustomerController.java
@@ -7,6 +7,7 @@ import com.petland.modules.customer.service.CustomerService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -18,11 +19,20 @@ public class CustomerController {
 
     private final CustomerService customerService;
     private final CustomerMapper customerMapper;
+    private final HttpServletRequest request;
 
-    @GetMapping
-    public ResponseEntity<CustomerResponseDTO> findById(HttpServletRequest request){
+    @GetMapping("/profile")
+    @PreAuthorize("hasRole('CUSTOMER')")
+    public ResponseEntity<CustomerResponseDTO> findProfile(){
         String customerId = request.getAttribute("id").toString();
         Customer customer = customerService.findCustomerById(UUID.fromString(customerId));
+        return ResponseEntity.ok().body(customerMapper.toDTO(customer));
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CustomerResponseDTO> findCustomerById(@PathVariable(name = "id") UUID customerId){
+        Customer customer = customerService.findCustomerById(customerId);
         return ResponseEntity.ok().body(customerMapper.toDTO(customer));
     }
 }
