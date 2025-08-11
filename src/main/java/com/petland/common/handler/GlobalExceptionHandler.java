427dba911cca22a01fa801d52fe9928256819828
Caique Pires <c.pires@ba.estudante.senai.br>
2025-08-11 15:12:34 +0100

    feat(appointment): Implements custom error handling
    - Handle unhandled errors globally
    - Handle exceptions thrown when entering an invalid date when scheduling an appointment
    - Handle exceptions when attempting to send a request with an invalid Http method

--- src/main/java/com/petland/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/petland/common/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.petland.common.error.ErrorResponseDTO;
 import com.petland.common.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -113,6 +114,39 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponseDTO(HttpStatus.BAD_REQUEST.value(),
                 "Invalid value for " + e.getName(), List.of(error)));
+    }
+
+    @ExceptionHandler(InvalidAppointmentTimeException.class)
+    public ResponseEntity<ErrorResponseDTO> handleInvalidAppointmentTime(InvalidAppointmentTimeException e){
+        ErrorMessageDTO errorMessageDTO = new ErrorMessageDTO("Invalid Date", e.getMessage());
+
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                HttpStatus.NOT_FOUND.value(),
+                e.getMessage(),
+                List.of(errorMessageDTO));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponseDTO> handleMethodNotSupported(HttpRequestMethodNotSupportedException e) {
+        ErrorMessageDTO errorField = new ErrorMessageDTO("URL", "Method Not Allowed");
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                HttpStatus.METHOD_NOT_ALLOWED.value(),
+                "Method not allowed for this endpoint.",
+                List.of(errorField)
+        );
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(error);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponseDTO> handleInternalError(RuntimeException e) {
+        ErrorMessageDTO error = new ErrorMessageDTO("Error", e.getClass().getSimpleName());
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "An unexpected error occurred",
+                List.of(error));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+
     }
 
 }
